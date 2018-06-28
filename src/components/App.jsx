5a48338b35cc0f7db8f41f049d65e1ed6b309f96
jmbice <jmbice@gmail.com>
2018-06-27 20:55:22 -0700

    Implements App component in React JS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,56 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstVideo: exampleVideoData[0],
+      allData: exampleVideoData
+    };
+  }
+
+  //future methods go here!
+
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><Search /></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><VideoPlayer data={this.state.firstVideo} /></div>
+          </div>
+          <div className="col-md-5">
+            <div><VideoList data={this.state.allData} /></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
+
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><Search /></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//        <div><VideoPlayer /></div>
+//       </div>
+//       <div className="col-md-5">
+//         <div><VideoList data={exampleVideoData} /></div>
+//       </div>
+//     </div>
+//   </div>
+// );
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,14 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.data.map(oneVideo =>
+      <VideoListEntry videoData={oneVideo} />
+    )}
   </div>
 );
+
+//array of props comes into VideoList
+//we then map these props to videoListEntries
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -17,3 +19,9 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
+{ /* <div><h5><em>videoListEntry</em> view goes here</h5></div>
+<div><h5><em>videoListEntry</em> view goes here</h5></div>
+<div><h5><em>videoListEntry</em> view goes here</h5></div>
+<div><h5><em>videoListEntry</em> view goes here</h5></div>
+<div><h5><em>videoListEntry</em> view goes here</h5></div> */ }
